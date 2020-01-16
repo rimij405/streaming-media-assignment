5773c078ed47d86947bfb3571f57175a5517f51b
Ian Effendi <effendiian@gmail.com>
2020-01-16 18:23:55 -0500

    Add support for streaming resources

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,89 +1,101 @@
 const fs = require('fs');
 const path = require('path');
 
-// Promise function to generalize checking if file exists.
-const findFile = (file) => {
-    return new Promise((resolve, reject) => {
-        fs.stat(file, (err, stats) => {
-            if(err) {
-                reject(err);
-            } else {
-                resolve(file, stats);
-            }
-        });
+// Check if the file exists at specified path. (Returns a promise)
+const fileExists = (filepath) => new Promise((resolve, reject) => {
+  // Asyncrhonous fs.stat call.
+  fs.stat(filepath, (err, stats) => {
+    if (!err) {
+      resolve(stats);
+    } else {
+      reject(err);
+    }
+  });
+});
+
+// Calculate the byte range.
+const byteRange = (range, size) => {
+  const positions = range.replace(/bytes=/, '').split('-');
+  let start = parseInt(positions[0], 10);
+  const total = size;
+  const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+  if (start > end) {
+    start = end - 1;
+  }
+
+  return {
+    start,
+    end,
+    total,
+  };
+};
+
+// Load file method.
+const loadFile = (request, response, filepath, mimetype) => {
+  // Resolve full pathname using directory.
+  const pathname = path.resolve(__dirname, filepath);
+
+  // If file exists at the specified file path. (Promise)
+  fileExists(pathname).then((stats) => {
+    // Get the range from the request headers.
+    let { range } = request.headers;
+
+    // Validate the range object.
+    if (!range) {
+      range = 'bytes=0-';
+    }
+
+    // Calculate the start and end positions.
+    const contentRange = byteRange(range, stats.size);
+
+    // Calculate the chunksize.
+    const chunksize = (contentRange.end - contentRange.start) + 1;
+
+    // Write the response headers.
+    response.writeHead(206, {
+      'Content-Range': `bytes ${contentRange.start}-${contentRange.end}/${contentRange.total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': mimetype,
     });
-}
+
+    // Create the file stream.
+    const stream = fs.createReadStream(pathname, contentRange);
+
+    stream.on('open', () => {
+      stream.pipe(response);
+    });
+
+    stream.on('error', (streamErr) => {
+      response.end(streamErr);
+    });
+
+    return stream;
+  }).catch((err) => {
+    // If a 404 error, write the header ...
+    if (err.code === 'ENOENT') {
+      response.writeHead(404);
+    }
+
+    // ... then end the response.
+    return response.end(err);
+  });
+};
+
+const getBird = (request, response) => {
+  loadFile(request, response, '../client/bird.mp4', 'video/mp4');
+};
 
 const getBling = (request, response) => {
-    const file = path.resolve(__dirname, '../client/bling.mp3');
-
-    // Does the fs.stat in the promise.
-    findFile(file).then((file, stats) => {
-
-    }).catch((err) => {
-        if(err.code === 'ENOENT'){
-            response.writeHead(404);
-        }
-        return response.end(err);
-    });
-
-}
+  loadFile(request, response, '../client/bling.mp3', 'audio/mpeg');
+};
 
 const getParty = (request, response) => {
-    const file = path.resolve(__dirname, '../client/party.mp4');
-
-    // Does the fs.stat in the promise.
-    findFile(file).then((file, stats) => {
-        
-        // Destructure statement for range.
-        // equivalent to 'let range = request.headers.range'
-        let { range } = request.headers;
-
-        if(!range){
-            range = 'bytes=0-';
-        }
-
-        const positions = range.replace(/bytes=/, '').split('-');
-
-        let start = parseInt(positions[0], 10);
-
-        const total = stats.size;
-        const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-
-        if(start > end){
-            start = end - 1;
-        }
-
-        const chunksize = (end - start) + 1;
-
-        response.writeHead(206, {
-            'Content-Range': `bytes ${start}-${end}/${total}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4',
-        });
-
-        const stream = fs.createReadStream(file, { start, end });
-
-        stream.on('open', () => {
-            stream.pipe(response);
-        });
-
-        stream.on('error', (streamErr) => {
-            response.end(streamErr);
-        });
-
-        return stream;
-
-    }).catch((err) => {
-        if(err.code === 'ENOENT'){
-            response.writeHead(404);
-        }
-        return response.end(err);
-    });
-}
+  loadFile(request, response, '../client/party.mp4', 'video/mp4');
+};
 
 module.exports = {
-    getBling,
-    getParty
-}
+  getBird,
+  getBling,
+  getParty,
+};
